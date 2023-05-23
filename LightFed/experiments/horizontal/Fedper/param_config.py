d335524bc96f3521a4b986620ccc611088945775
lkyddd <72898970+lkyddd@users.noreply.github.com>
2023-05-23 20:46:14 +0800

    Update param_config.py

--- LightFed/experiments/horizontal/Fedper/param_config.py
+++ LightFed/experiments/horizontal/Fedper/param_config.py
@@ -13,34 +13,21 @@ def get_args():
 
     parser.add_argument('--comm_round', type=int, default=200)
 
-    parser.add_argument('--I', type=int, default=1, help='synchronization interval, 本地训练步数')
+    parser.add_argument('--I', type=int, default=1, help='synchronization interval')
 
-    parser.add_argument('--batch_size', type=int, default=64)  # 128 训练的batch
+    parser.add_argument('--batch_size', type=int, default=64)  # 
 
-    # parser.add_argument('--gen_batch_size', type=int, default=64)  # 128 训练的batch
-
-
-    # 多少个通信轮次评估一次全局模型
-    # 默认1表示每个通信轮次结束后都要评估模型
     parser.add_argument('--eval_step_interval', type=int, default=5)
-    # 每个通信轮次完成后，评估全局模型时的全量数据的dataloader的批次大小
-    # 可以适当大一点
     parser.add_argument('--eval_batch_size', type=int, default=256)
-
-    # 每个通信轮次完成后，评估全局模型时是否使用全量的测试集（训练集的评估固定只取一个批次，批次大小为：eval_batch_size）
-    # 如果为True，则全量评估测试集，否则只评估一个批次，批次的大小为：eval_batch_size
     parser.add_argument('--eval_on_full_test_data', type=lambda s: s == 'true', default=True)
 
     parser.add_argument('--weight_decay', type=float, default=0.0)
 
-    ### 本地更新的相关参数
-    ## 学习率
     parser.add_argument('--lr_lm', type=float, default=0.001, help='learning rate of local model')
     parser.add_argument('--I_lm', type=int, default=20, help='learning rate of local model')
 
     parser.add_argument("--embedding", type=bool, default=False, help="Use embedding layer in generator network")
 
-    # 模型权重衰减系数，只对权重参数进行衰减，偏置参数不加L2正则
     parser.add_argument('--model_type', type=str, default='Lenet', choices=['Lenet', 'ResNet_18', 'ResNet_20'])
 
     parser.add_argument('--data_set', type=str, default='EMNIST',
@@ -49,7 +36,7 @@ def get_args():
     parser.add_argument('--data_partition_mode', type=str, default='non_iid_dirichlet_balanced',
                         choices=['iid', 'non_iid_dirichlet_unbalanced', 'non_iid_dirichlet_balanced']) #'non_iid_dirichlet',
 
-    parser.add_argument('--non_iid_alpha', type=float, default=0.1)  # 在进行non_iid_dirichlet数据划分时需要, 该值越大表明数据越均匀
+    parser.add_argument('--non_iid_alpha', type=float, default=0.1)  
 
     parser.add_argument('--client_num', type=int, default=10)
 
